b354e531c5c5c32310fb65b3fc3bf6126f9dc944
Ayush <khatriayush2003@gmail.com>
2023-07-24 23:29:57 +0530

    added website logo and some ui changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 		<footer className=' rounded-lg shadow m-4 bg-zinc-950 mt-20'>
 			<hr className='opacity-20' />
 			<div className='w-full mx-auto max-w-screen-xl  justify-center p-4 md:flex items-center '>
-				<ul className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0'>
+				<ul className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  sm:mt-0'>
 					<li>
 						<span className='mr-4  md:mr-6 '>
 							Developed and Designed by{" "}
@@ -20,6 +20,7 @@ const Footer = () => {
 						</span>
 					</li>
 				</ul>
+							<a href="https://www.flaticon.com/free-icons/app" className="text-gray-100 text-sm md:text-base  " title="app icons">App icons created by Freepik - Flaticon</a>
 			</div>
 		</footer>
 	);
